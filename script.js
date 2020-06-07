a637c723f5fc9d8bb0a2a8f1a5fd96d3c4d58d9e
Ibrahim Amusan <ibamusan@gmail.com>
2020-06-08 00:26:40 +0100

    setting up edabit javascript challenges

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a function that takes height and width and finds the perimeter of a rectangle.
+
+function findPerimeterRectangle(height, width){
+  var x = height;
+  var y = width
+  return (2*x + 2*y);
+}
+
+findPerimeterRectangle(2,3);
+
