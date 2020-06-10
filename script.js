402d5cf98fa8ddc5add1b60f3834b7f4aba4f136
Ibrahim Amusan <ibamusan@gmail.com>
2020-06-10 10:49:16 +0100

    11th challenge added

--- script.js
+++ script.js
@@ -130,3 +130,23 @@ function testLogic2(x,y){
 }
 
 testLogic2(3,5);
+
+
+// In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. 
+// The farmer breeds three species:
+
+// chickens = 2 
+// legscows = 4 legs
+// pigs = 4 legs
+// The farmer has counted his animals and he gives you a subtotal for each species.
+// You have to implement a function that returns the total number of legs of all the animals.
+
+function calcTotalLegs(chickens, cows, pigs){
+  x = chickens * 2;
+  y = cows * 4;
+  z = pigs * 4;
+
+  return (x+y+z);
+}
+
+console.log(calcTotalLegs(4,6,10));
