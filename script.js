9f000f1c88be40cd2b7b2add8cf6753771dddd5d
Ibrahim Amusan <ibamusan@gmail.com>
2020-06-14 22:02:56 +0100

    32nd challenge added

--- script.js
+++ script.js
@@ -391,3 +391,11 @@ function divisibleByFive(int){
 }
 
 console.log(divisibleByFive(585));
+
+// Create a function to concatenate two integer arrays.
+
+function arrayConcat(arr1, arr2){
+  return arr1.concat(arr2);
+}
+
+console.log(arrayConcat([1,2,3], [4,5,6,7]));
