3ceb5900733b5dad93cd39f616edcb38aa58ab5d
Ibrahim Amusan <ibamusan@gmail.com>
2020-06-18 23:45:25 +0100

    1st challenge  added

--- day17.js
+++ day17.js
@@ -57,5 +57,23 @@ console.log(helloGreating('ibrahim'));
 
 
 
+// Create a function that takes an object as an argument and returns a string with facts about the city.
+//  The city facts will need to be extracted from the object's three properties:
+
+// name
+// population
+// continent
+// The string should have the following format: X has a population of Y and is situated in Z 
+// (where X is the city name, Y is the population and Z is the continent the city is situated in).
 
 
+function cityFacts(city) {
+	return city.name + " has a population of " + city.population + " and is situation in " + city.continent;
+}
+
+
+console.log(cityFacts({
+name: "ade",
+population: "234788",
+continent:"african",
+}));
