f8cae361c5de529661d22b710761fe40470eab37
Ibrahim Amusan <ibamusan@gmail.com>
2020-06-20 17:48:03 +0100

    2nd challenge added

--- day20.js
+++ day20.js
@@ -5,3 +5,19 @@ function reArray(arr){
 }
 
 console.log(reArray([1,2,3,4,5,6]));
+
+
+//Create a function that searches for the index of a given item in an array. 
+// If the item is present, it should return the index, otherwise, it should return -1.
+
+function findSindex(arr, n){
+  let y = arr.indexOf(n)
+
+  if (y != -1){
+    return y;
+  }else {
+    return -1;
+  }
+}
+
+console.log(findSindex([1,2,3,4,5], 4));
