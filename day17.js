e271faf6c849e6c1961c1a7dadf0d7958cdb4965
Ibrahim Amusan <ibamusan@gmail.com>
2020-06-25 17:41:16 +0100

    1st challenge added

--- day17.js
+++ day17.js
@@ -126,3 +126,18 @@ function validStrNumber(n) {
 	}
 }
 
+
+// Create a function that takes in a current mood and return a sentence in the following format: 
+// "Today, I am feeling {mood}". However, if no argument is passed, return "Today, I am feeling neutral".
+
+function retSent(mood){
+	
+	if (mood){
+		return "Today, I am feeling " + mood;
+}else{
+	return  "Today, I am feeling neutral";
+}
+	
+}
+
+console.log(retSent("good"));
