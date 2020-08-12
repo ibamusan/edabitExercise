b8bae194dbba280366d1508c3fd3da06f4b2925b
Ibrahim Amusan <ibamusan@gmail.com>
2020-08-12 13:04:15 +0100

    edabit challenge on conversion of array to strings

--- edabitChallenge2.js
+++ edabitChallenge2.js
@@ -5,3 +5,12 @@ function giveMesomething(a){
 }
 
 console.log(giveMesomething("good is better"));
+
+
+// Create a function that takes an array of numbers or letters and returns a string.
+
+function convertArraytoString(arr){
+  return arr.join("")
+}
+
+console.log(convertArraytoString([1,2,3,4,5,6]));
