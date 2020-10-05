93168c733cec9391b3eac0ba96f2aaada68d0a18
Ibrahim Amusan <ibamusan@gmail.com>
2020-10-05 19:55:16 +0100

    creating an array from two given argument

--- edabitChallenge1.js
+++ edabitChallenge1.js
@@ -14,3 +14,13 @@ function calcPower(I,V){
 
 console.log(calcPower(4,8));
 console.log(calcPower(10,9));
+
+// Given two arguments, return an array which contains these two arguments.
+
+function cretArray (x,y){
+  let arr = []
+  arr.push(x,y);
+  return arr;
+}
+
+console.log(cretArray(2,3));
