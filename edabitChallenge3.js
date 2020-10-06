797d8825a05b7218e3e7c048f74a411d032ee20f
Ibrahim Amusan <ibamusan@gmail.com>
2020-10-06 20:12:31 +0100

    function to check for length of a string

--- edabitChallenge3.js
+++ edabitChallenge3.js
@@ -4,3 +4,15 @@ function convertAge(age){
 }
 
 console.log(convertAge(70));
+
+// Given a string, return true if its length is even or false if the length is odd.
+
+function stringTest(str){
+  if((str.length % 2) == 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(stringTest('apple'));
