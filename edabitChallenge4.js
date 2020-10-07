255a8183a9e2bd6b20e6142f94bec9614e8d2cfb
Ibrahim Amusan <ibamusan@gmail.com>
2020-10-07 20:22:32 +0100

    function to convert inches to feet but return zoro if inches is less 12

--- edabitChallenge4.js
+++ edabitChallenge4.js
@@ -4,3 +4,16 @@ function inchesToFeet(inches){
 }
 
 console.log(inchesToFeet(1));
+
+// Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet. 
+// Note: return 0 if inches is less than 12
+function inchesToFt(inches){
+  if (inches >= 12){
+    return inches/12 ;
+  }else {
+    return 0
+  }
+}
+
+console.log(inchesToFt(1));
+
