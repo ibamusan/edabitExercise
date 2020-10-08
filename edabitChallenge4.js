a62e9754a7cee46fbc34faa4c3d26af08f91c7de
Ibrahim Amusan <ibamusan@gmail.com>
2020-10-08 20:59:58 +0100

    fix bug

--- edabitChallenge4.js
+++ edabitChallenge4.js
@@ -17,3 +17,23 @@ function inchesToFt(inches){
 
 console.log(inchesToFt(1));
 
+// Write a function that checks whether a person can watch an MA15+ rated movie. 
+// One of the following two conditions is required for admittance:
+// The person is at least 15 years old.
+// They have parental supervision.
+// The function accepts two parameters, age and isSupervised. Return a boolean.
+
+
+
+
+// The challenge is to try and fix this buggy code, given the inputs true and false. See the examples below for the expected output.
+
+  function has_bugs(buggy_code) {
+    if (buggy_code) {
+      return `sad days`
+    } else{
+      return `it's a good day`
+    }
+  }
+
+//its just to fix the code, its has bug on the edabite website but fix here.
